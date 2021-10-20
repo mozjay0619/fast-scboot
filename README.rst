75e5cc7ab0067ebb97804f9d365ef67e781911d3
Jay Kim <jkim2@grubhub.com>
2021-10-20 14:05:35 +0900

    fix pytest

--- README.rst
+++ README.rst
@@ -5,4 +5,9 @@ FastScboot
 
 FastScboot is a statistics tool to perform the stratified clustered bootstrap sampling on given data. The algorithm is fast in the sense that the remaining bottleneck to the speed of the algorithm is the speed of memory access during the inplace fancy indexing operation.
 
- 
+Install
+-------
+
+::
+
+	pip install fast-scboot
--- tests/test_sample_index_helper.py
+++ tests/test_sample_index_helper.py
@@ -2,16 +2,16 @@ import pytest
 
 import numpy as np
 
-from src.fast_scboot.c.sample_index_helper import (count_clusts,
+from src.fast_scboot.c.sample_index_helper import (count_clust_array,
                                                    make_index_matrix)
 
 
 def test_make_index_matrix():
 
-    strats = np.asarray([0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2])
-    clusts = np.asarray([0, 1, 1, 2, 3, 4, 4, 5, 6, 6, 6])
+    strat_array = np.asarray([0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2])
+    clust_array = np.asarray([0, 1, 1, 2, 3, 4, 4, 5, 6, 6, 6])
     clust_val = np.asarray([0, 1, 1, 2, 3, 4, 4, 5, 6, 6, 6])
-    array = np.squeeze(np.dstack([strats, clusts, clust_val])).astype(np.int32)
+    array = np.squeeze(np.dstack([strat_array, clust_array, clust_val])).astype(np.int32)
 
     result = make_index_matrix(array, 7)
 
@@ -29,9 +29,9 @@ def test_make_index_matrix():
 
     assert np.all(np.isclose(result, answer))
 
-    strats = np.asarray([0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2])
-    clusts = np.asarray([0, 1, 1, 2, 3, 4, 4, 5, 6, 6, 7])
-    array = np.squeeze(np.dstack([strats, clusts, clusts])).astype(np.int32)
+    strat_array = np.asarray([0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2])
+    clust_array = np.asarray([0, 1, 1, 2, 3, 4, 4, 5, 6, 6, 7])
+    array = np.squeeze(np.dstack([strat_array, clust_array, clust_array])).astype(np.int32)
 
     result = make_index_matrix(array, 8)
 
@@ -51,63 +51,48 @@ def test_make_index_matrix():
     assert np.all(np.isclose(result, answer))
 
 
-def test_count_clusts():
+def test_count_clust_array():
 
-    strats = np.asarray([0, 0, 1, 1, 1])
-    clusts = np.asarray([0, 1, 2, 2, 3])
-    array = np.squeeze(np.dstack([strats, clusts, clusts, clusts, clusts])).astype(
-        np.int32
-    )
+    strat_array = np.asarray([0, 0, 1, 1, 1]).astype(np.int32)
+    clust_array = np.asarray([0, 1, 2, 2, 3]).astype(np.int32)
 
-    result = count_clusts(array, 2, len(array))
+    result = count_clust_array(strat_array, clust_array, 2, len(clust_array))
 
     answer = np.asarray([2, 2])
 
     assert np.all(np.isclose(result, answer))
 
-    strats = np.asarray([0, 0, 1, 1, 1])
-    clusts = np.asarray([0, 1, 2, 3, 4])
-    array = np.squeeze(np.dstack([strats, clusts, clusts, clusts, clusts])).astype(
-        np.int32
-    )
+    strat_array = np.asarray([0, 0, 1, 1, 1]).astype(np.int32)
+    clust_array = np.asarray([0, 1, 2, 3, 4]).astype(np.int32)
 
-    result = count_clusts(array, 2, len(array))
+    result = count_clust_array(strat_array, clust_array, 2, len(clust_array))
 
     answer = np.asarray([2, 3])
 
     assert np.all(np.isclose(result, answer))
 
-    strats = np.asarray([0, 0, 1, 1, 2])
-    clusts = np.asarray([0, 1, 2, 3, 4])
-    array = np.squeeze(np.dstack([strats, clusts, clusts, clusts, clusts])).astype(
-        np.int32
-    )
+    strat_array = np.asarray([0, 0, 1, 1, 2]).astype(np.int32)
+    clust_array = np.asarray([0, 1, 2, 3, 4]).astype(np.int32)
 
-    result = count_clusts(array, 3, len(array))
+    result = count_clust_array(strat_array, clust_array, 3, len(clust_array))
 
     answer = np.asarray([2, 2, 1])
 
     assert np.all(np.isclose(result, answer))
 
-    strats = np.asarray([0, 0, 1, 1, 1])
-    clusts = np.asarray([0, 1, 3, 3, 3])
-    array = np.squeeze(np.dstack([strats, clusts, clusts, clusts, clusts])).astype(
-        np.int32
-    )
+    strat_array = np.asarray([0, 0, 1, 1, 1]).astype(np.int32)
+    clust_array = np.asarray([0, 1, 3, 3, 3]).astype(np.int32)
 
-    result = count_clusts(array, 2, len(array))
+    result = count_clust_array(strat_array, clust_array, 2, len(clust_array))
 
     answer = np.asarray([2, 1])
 
     assert np.all(np.isclose(result, answer))
 
-    strats = np.arange(5)
-    clusts = np.arange(5)
-    array = np.squeeze(np.dstack([strats, clusts, clusts, clusts, clusts])).astype(
-        np.int32
-    )
+    strat_array = np.arange(5).astype(np.int32)
+    clust_array = np.arange(5).astype(np.int32)
 
-    result = ccount_clusts(array, 5, len(array))
+    result = count_clust_array(strat_array, clust_array, 5, len(clust_array))
 
     answer = np.asarray([1, 1, 1, 1, 1])
 
